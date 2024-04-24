3e7ab42cc6d50a8eb6fc1d04172d4b7ac10676e8
wamuM <el.mail.de.wamu.m@gmail.com>
2024-04-24 18:44:44 +0200

    Added function headers

---
+++ solucio.cc
@@ -0,0 +1,21 @@
+#include "Cjt_estudiants.hh" 
+void afegir_estudiant(const Estudiant &est, bool& b);
+/* Pre: el paràmetre implícit no està ple */
+/* Post: b = indica si el p.i. original conté un estudiant amb el dni d'est;
+   si b = fals, s'ha afegit l'estudiant est al paràmetre implícit */
+
+void esborrar_estudiant(int dni, bool& b);
+/* Pre: cert */
+/* Post: b indica si el paràmetre implícit original tenia un estudiant 
+   amb el dni dni; si b, aquest estudiant ha quedat eliminat
+   del paràmetre implícit */
+
+void incrementar_interval(double x);
+/* Pre: x és una nota vàlida */
+/* Post: al paràmetre implícit, s'ha incrementat en una unitat el valor de
+   la posició d'"intervals" corresponent a x */
+
+void decrementar_interval(double x);
+/* Pre: x és una nota vàlida */
+/* Post: al paràmetre implícit, s'ha decrementat en una unitat el valor de
+   la posició d'"intervals" corresponent a x */
