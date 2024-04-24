11ca76ea0d2eed8d4454788322cfcb06e0fe4196
wamuM <el.mail.de.wamu.m@gmail.com>
2024-04-24 19:32:05 +0200

    Afegir estudiant

--- solucio_b.cc
+++ solucio_b.cc
@@ -6,13 +6,20 @@
 /* Post: b = indica si el p.i. original conté un estudiant amb el dni d'est;
    si b = fals, s'ha afegit l'estudiant est al paràmetre implícit */
 void Cjt_estudiants::afegir_estudiant(const Estudiant &est, bool& b){
+    int index = Cjt_estudiants::cerca_dicot(this->vest,0, this->mida(), est.consultar_DNI());
+    if(this->vest[index].consultar_DNI() == est.consultar_DNI()){
+        b = true;
+    }else{
+      this->vest.insert(index, est);
+      if(est.te_nota())this->incrementar_interval(est.consultar_nota());
+    }
 }
 
 /* Pre: cert */
 /* Post: b indica si el paràmetre implícit original tenia un estudiant 
    amb el dni dni; si b, aquest estudiant ha quedat eliminat
    del paràmetre implícit */
-void Cjt_estuditants::esborrar_estudiant(int dni, bool& b){
+void Cjt_estudiants::esborrar_estudiant(int dni, bool& b){
 
 }
 
@@ -28,7 +35,7 @@ void Cjt_estudiants::incrementar_interval(double x){
 /* Pre: x és una nota vàlida 
 /* Post: al paràmetre implícit, s'ha decrementat en una unitat el valor de
    la posició d'"intervals" corresponent a x */
-void Cjt_esutdiants::decrementar_interval(double x){
+void Cjt_estudiants::decrementar_interval(double x){
     int floor = x; 
     if(floor == 10)x = 9; 
     --this->intervals[floor];
